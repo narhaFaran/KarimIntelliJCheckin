94ef76e97a6863a78a38d195f748d4dca2b1cdab
narhaF <farhank9@nycstudents.net>
2023-09-27 12:05:24 -0400

    initial commit

---
+++ src/ClassTest.java
@@ -0,0 +1,6 @@
+public class ClassTest {
+    public static void main(String[] args) {
+        System.out.println("Is this the Krusty Krab?");
+        System.out.print("No, this is Patrick.");
+    }
+}
